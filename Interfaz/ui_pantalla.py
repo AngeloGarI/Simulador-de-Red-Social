31fa3e93c753cd376cd8a082725e8030099e6539
Angelo <27garciaangelo@gmail.com>
2026-03-22 01:16:46 -0600

    Sin cambios

--- Interfaz/ui_pantalla.py
+++ Interfaz/ui_pantalla.py
@@ -1,32 +1,22 @@
 from PyQt6.QtWidgets import *
 
 class Pantalla(QWidget):
-
     def __init__(self,main):
-
         super().__init__()
-
         self.main = main
-
         layout = QVBoxLayout()
-
         btn_dark = QPushButton("Modo oscuro")
         btn_dark.clicked.connect(self.dark)
-
         btn_light = QPushButton("Modo claro")
         btn_light.clicked.connect(self.light)
-
         layout.addWidget(btn_dark)
         layout.addWidget(btn_light)
-
         self.setLayout(layout)
 
     def dark(self):
-
         self.main.setStyleSheet(
         "background:#18191a;color:white;"
         )
 
     def light(self):
-
         self.main.setStyleSheet("")
